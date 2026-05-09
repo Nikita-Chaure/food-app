f59fc1ae744973351344b1561c460d82f177719e
Nikita-Chaure <167289257+Nikita-Chaure@users.noreply.github.com>
2026-05-09 23:19:09 +0530

    n

--- app.py
+++ app.py
@@ -100,4 +100,6 @@ def smart_match():
 # ---------------- RUN ----------------
 
     if __name__ == "__main__":
-        app.run(host="0.0.0.0", port=8000)
+        import os
+        port = int(os.environ.get("PORT", 8000))
+        app.run(host="0.0.0.0", port=port)
